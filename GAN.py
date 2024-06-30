ee51397a1e429d095395f4be417251994bcebbdd
Vahid <vahid_negahdari@outlook.com>
2024-07-01 01:46:21 +0330

    1

--- GAN.py
+++ GAN.py
@@ -111,7 +111,7 @@ def train_step(Real,lr1,lr2):
     optimizer_D = tf.keras.optimizers.Adam(learning_rate=lr2)
 
     Noise = np.random.normal(0,1,[batch_size,k] ).astype('float32')
-    with tf.GradientTape() as gen_tape , tf.GradientTape() as disc_tape:
+    with tf.GradientTape(persistent=True) as gen_tape , tf.GradientTape(persistent=True) as disc_tape:
         Fake        = Generator(Noise)
         Real_output = Discriminator(Real)
         Fake_output = Discriminator(Fake)
