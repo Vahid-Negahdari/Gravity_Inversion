d795220da22c3ba9ca22ef8e2f335ed9c58733a6
Vahid <vahid_negahdari@outlook.com>
2024-07-02 00:59:20 +0330

    1

--- GAN.py
+++ GAN.py
@@ -13,7 +13,7 @@ num_batch    = int(BIGG_BATCH/batch_size)
 lr1          = 0.0005
 lr2          = 0.0005
 n            = 51
-k            = 200
+k            = 100
 semi         = int(np.ceil(n/8)*np.ceil(n/8)*128)
 #########################################################
 # Import Data
@@ -30,7 +30,7 @@ def conv(input, w, stride, dimention):
        y = tf.nn.conv1d(input=input, filters=w, stride=stride,padding='SAME')
     else :
        y = tf.nn.conv2d(input=input, filters=w, strides=stride, padding='SAME')
-    y = tf.nn.leaky_relu(y)
+    y = tf.nn.relu(y)
     return y
 
 
@@ -39,7 +39,7 @@ def deconv(input, w, strides, output, dimention):
        y = tf.nn.conv1d_transpose(input=input, filters=w, strides=strides, padding='SAME', output_shape=output)
     else:
        y = tf.nn.conv2d_transpose(input=input, filters=w, strides=strides, padding='SAME', output_shape=output)
-    y = tf.nn.leaky_relu(y)
+    y = tf.nn.relu(y)
     return y
 
 
