3c8e8eddb160e1b8715a518b0547120ea23d910b
Vahid <vahid_negahdari@outlook.com>
2024-07-11 00:11:57 +0330

    1

--- Create_Dataset.py
+++ Create_Dataset.py
@@ -3,7 +3,7 @@ import numpy as np
 from pathlib import Path
 
 
-path = Path('G:\projet\Gravity Density Inversion')
+path = Path('/home/cvl/Pycharm/Gravity_Density_Inversion')
 #########################################################
 # Set Domain Node
 #########################################################
