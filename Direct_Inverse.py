6ab1df8281d5f51d1bc8e0259777802cfa3523f0
Vahid <vahid_negahdari@outlook.com>
2024-07-12 18:15:46 +0330

    1

--- Direct_Inverse.py
+++ Direct_Inverse.py
@@ -20,7 +20,7 @@ semi2        = int((n**2)/2)
 #########################################################
 A       = tf.transpose(tf.constant(np.load(path / ('A.npy'), allow_pickle=True)))
 Density = 1-np.load(path / ('Density.npy'), allow_pickle=True)
-Gravity       = np.load(path / ('Gravity.npy'), allow_pickle=True)
+Gravity       = np.load(path / ('Gravity.npy'), allow_pickle=True)/100
 #Gravity = np.zeros([27000,n,2]).astype('float32')
 #Gravity[:,:,0] = G[:,0:n]     ;    Gravity[:,:,1] = G[:,n:2*n]
 Gravity = np.expand_dims(Gravity,axis=2)
